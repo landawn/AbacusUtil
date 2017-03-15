8aab1d44df90cb9ba92a0c5147a6870554ea23d9
landawn <landawn@users.noreply.github.com>
2017-03-15 08:09:22 -0700

    0.9.58

--- src/com/landawn/abacus/util/Seq.java
+++ src/com/landawn/abacus/util/Seq.java
@@ -2242,6 +2242,47 @@ public final class Seq<T> implements Collection<T> {
         return newValue;
     }
 
+    public static <T> Iterator<T> skipNull(final Iterator<? extends T> iter) {
+        return new Iterator<T>() {
+            private T next;
+
+            @Override
+            public boolean hasNext() {
+                if (next == null && iter.hasNext()) {
+                    next = iter.next();
+
+                    if (next == null) {
+                        while (iter.hasNext()) {
+                            next = iter.next();
+
+                            if (next != null) {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return next != null;
+            }
+
+            @Override
+            public T next() {
+                if (next == null && hasNext() == false) {
+                    throw new NoSuchElementException();
+                }
+
+                final T result = next;
+                next = null;
+                return result;
+            }
+
+            @Override
+            public void remove() {
+                iter.remove();
+            }
+        };
+    }
+
     /**
      * Note: copy from Google Guava under Apache License v2.
      * <br />
