26a55200468caca4eebdbd8f20e3743ed8a370c0
landawn <landawn@users.noreply.github.com>
2017-03-15 08:39:19 -0700

    samples

--- samples/com/landawn/samples/abacus/DataSetTest.java
+++ samples/com/landawn/samples/abacus/DataSetTest.java
@@ -1,13 +1,11 @@
 package com.landawn.samples.abacus;
 
-import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
 
 import com.landawn.abacus.DataSet;
 import com.landawn.abacus.util.N;
-import com.landawn.abacus.util.stream.IntStream;
 import com.landawn.samples.abacus.entity.Account;
 import com.landawn.samples.abacus.entity.Contact;
 
@@ -16,17 +14,15 @@ public class DataSetTest {
 
     @Test
     public void test_join() {
-        List<Account> accounts = N.asList(N.fill(Account.class), N.fill(Account.class), N.fill(Account.class));
-        List<Contact> contacts = N.asList(N.fill(Contact.class), N.fill(Contact.class), N.fill(Contact.class));
+        Account account = new Account().setId(1001).setFirstName("Tom");
+        Contact contact = new Contact().setId(2001).setAccountId(1001).setAddress("1 Rd");
 
-        IntStream.range(0, N.min(accounts.size(), contacts.size())).forEach(i -> contacts.get(i).setAccountId(accounts.get(i).getId()));
-
-        DataSet ds1 = N.newDataSet(accounts);
+        DataSet ds1 = N.newDataSet(N.asList("id", "firstName"), N.asList(account));
         ds1.println();
 
-        DataSet ds2 = N.newDataSet(contacts);
+        DataSet ds2 = N.newDataSet(N.asList("id", "accountId", "address"), N.asList(contact));
         ds2.println();
-        ds2.renameColumn(N.asList("id", "status", "lastUpdateTime", "createTime"), e -> "contact" + N.capitalize(e));
+        ds2.renameColumn("id", "contactId");
 
         DataSet ds3 = ds1.join(ds2, N.asMap("id", "accountId"));
         ds3.println();
